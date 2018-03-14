0f983b07dc05232e56a99ac3eca91784545b426b
philipbeel <philipbeel@googlemail.com>
2018-03-14 22:15:14 +0000

    [whitespace]

---
+++ ts-node-ed017c78147c47f55e1e5542e54e6fa738919f1d0cb7221a94df7253a2cf6a16/feb1031e3bfc44a86d1af5d2473a23071be14e8bd9a35e49fefce4790003bc1b/1aa07a3dd4a2bb2cee12e6222f1e3b1549816ee31c521346b6c0c86dd0956c16.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Add = /** @class */ (function () {
+    function Add() {
+        console.log('Addition module');
+    }
+    Add.prototype.add = function (firstNumber, secondNumber) {
+        return (firstNumber + secondNumber);
+    };
+    return Add;
+}());
+exports.default = Add;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiL1VzZXJzL3BoaWxpcGJlZWwvU2l0ZXMvZ2l0aHViL2V4YW1wbGUtdHlwZXNjcmlwdC1ueWMtbW9jaGEtY292ZXJhZ2UvY2FsY3VsYXRvci90cy9zcmMvQWRkLnRzIiwic291cmNlcyI6WyIvVXNlcnMvcGhpbGlwYmVlbC9TaXRlcy9naXRodWIvZXhhbXBsZS10eXBlc2NyaXB0LW55Yy1tb2NoYS1jb3ZlcmFnZS9jYWxjdWxhdG9yL3RzL3NyYy9BZGQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQTtJQUVJO1FBQ0ksT0FBTyxDQUFDLEdBQUcsQ0FBQyxpQkFBaUIsQ0FBQyxDQUFDO0lBQ25DLENBQUM7SUFFTSxpQkFBRyxHQUFWLFVBQVcsV0FBbUIsRUFBRSxZQUFvQjtRQUNoRCxNQUFNLENBQUMsQ0FBQyxXQUFXLEdBQUcsWUFBWSxDQUFDLENBQUM7SUFDeEMsQ0FBQztJQUNMLFVBQUM7QUFBRCxDQUFDLEFBVEQsSUFTQyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IGNsYXNzIEFkZCB7XG5cbiAgICBjb25zdHJ1Y3RvcigpIHtcbiAgICAgICAgY29uc29sZS5sb2coJ0FkZGl0aW9uIG1vZHVsZScpO1xuICAgIH1cblxuICAgIHB1YmxpYyBhZGQoZmlyc3ROdW1iZXI6IG51bWJlciwgc2Vjb25kTnVtYmVyOiBudW1iZXIpOiBudW1iZXIge1xuICAgICAgICByZXR1cm4gKGZpcnN0TnVtYmVyICsgc2Vjb25kTnVtYmVyKTtcbiAgICB9XG59XG4iXX0=
